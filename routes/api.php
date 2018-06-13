0dac02b3fd4e864a2f7978771ab75c9d344593ae
tanbin <1370808424@qq.com>
2018-06-13 11:33:03 +0800

    user can register using phone

--- routes/api.php
+++ routes/api.php
@@ -8,5 +8,7 @@ $api->version('v1', [
     'namespace' => 'App\Http\Controllers\Api'
 ], function ($api) {
     // 短信验证码
-    $api->post('verificationCodes', 'verificationCodesController@store')->name('api.verificationCodes.store');
+    $api->post('verificationCodes', 'VerificationCodesController@store')->name('api.verificationCodes.store');
+    // 用户注册
+    $api->post('users', 'UsersController@store')->name('api.users.store');
 });

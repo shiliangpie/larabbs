60c56a5f487838499f9d1cc9f2b742a28f7595be
tanbin <1370808424@qq.com>
2018-06-19 14:34:39 +0800

    social authorizations

--- routes/api.php
+++ routes/api.php
@@ -13,10 +13,16 @@ $api->version('v1', [
         'expires' => config('api.rate_limits.access.expires'),
     ], function ($api) {
         // 短信验证码
-        $api->post('verificationCodes', 'VerificationCodesController@store')->name('api.verificationCodes.store');
+        $api->post('verificationCodes', 'VerificationCodesController@store')
+            ->name('api.verificationCodes.store');
         // 用户注册
-        $api->post('users', 'UsersController@store')->name('api.users.store');
+        $api->post('users', 'UsersController@store')
+            ->name('api.users.store');
         // 图片验证码
-        $api->post('captchas', 'CaptchasController@store')->name('api.captchas.store');
+        $api->post('captchas', 'CaptchasController@store')
+            ->name('api.captchas.store');
+        // 第三方登录
+        $api->post('socials/{social_type}/authorizations', 'AuthorizationsController@socialStore')
+            ->name('api.socials.authorizations.store');
     });
 });

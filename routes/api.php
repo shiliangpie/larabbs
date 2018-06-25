a900fdb4b9ffdee52114ff53c662082c943beb18
tanbin <1370808424@qq.com>
2018-06-25 15:57:16 +0800

    replies index

--- routes/api.php
+++ routes/api.php
@@ -47,6 +47,12 @@ $api->version('v1', [
         // 某个用户发布的所有话题
         $api->get('users/{user}/topics', 'TopicsController@userIndex')
             ->name('api.users.topics.index');
+        // 话题回复列表
+        $api->get('topics/{topic}/replies', 'RepliesController@index')
+            ->name('api.topics.replies.index');
+        // 某个用户的回复列表
+        $api->get('users/{user}/replies', 'RepliesController@userIndex')
+            ->name('api.users.replies.index');
 
         // 需要token验证的接口
         $api->group(['middleware' => 'api.auth'], function ($api) {

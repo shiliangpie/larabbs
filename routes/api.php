d2a94985f41f96c8360285002eccb4b4889da185
tanbin <1370808424@qq.com>
2018-06-25 14:54:14 +0800

    topics show

--- routes/api.php
+++ routes/api.php
@@ -41,6 +41,9 @@ $api->version('v1', [
         // 话题列表
         $api->get('topics', 'TopicsController@index')
             ->name('api.topics.index');
+        // 话题详情
+        $api->get('topics/{topic}', 'TopicsController@show')
+            ->name('api.topics.show');
         // 某个用户发布的所有话题
         $api->get('users/{user}/topics', 'TopicsController@userIndex')
             ->name('api.users.topics.index');
